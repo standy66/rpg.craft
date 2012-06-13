82660fb9936e57701c6168411083d1f2fe473197
standy66 <steam.andy@gmail.com>
2012-06-13 14:11:01 +0400

    Added main class

---
+++ src/ru/darkcraft/RPGCraft/RPGCraft.java
@@ -0,0 +1,26 @@
+package ru.darkcraft.RPGCraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: KarN
+ * Date: 13.06.12
+ * Time: 14:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class RPGCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+    }
+
+}
