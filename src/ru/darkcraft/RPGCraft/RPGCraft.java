b854100f159df45cfaa495e79abed8735ed4c38c
standy66 <steam.andy@gmail.com>
2012-06-13 14:43:45 +0400

    onEnable() & onDisable implementation

--- src/ru/darkcraft/RPGCraft/RPGCraft.java
+++ src/ru/darkcraft/RPGCraft/RPGCraft.java
@@ -1,25 +1,39 @@
 package ru.darkcraft.RPGCraft;
 
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Created with IntelliJ IDEA.
+ * Main plugin class
  * User: KarN
  * Date: 13.06.12
  * Time: 14:03
- * To change this template use File | Settings | File Templates.
+ * @author KarN
+ * @version 1.0
+ *
  */
 public class RPGCraft extends JavaPlugin {
 
+    private Logger logger;
+    private PluginDescriptionFile pluginDescriptionFile;
+
+
     @Override
     public void onEnable() {
-
+        pluginDescriptionFile = getDescription();
+        logger = this.getLogger();
+        logger.log(Level.INFO,  pluginDescriptionFile.getName() + " " + pluginDescriptionFile.getVersion() + " is now enabled");
 
     }
 
     @Override
     public void onDisable() {
 
+        logger.log(Level.INFO,  pluginDescriptionFile.getName() + " " + pluginDescriptionFile.getVersion() + " is now enabled");
 
     }
 
