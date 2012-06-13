cd8b6f137a682c9120ba6ed3a110d2398f3df695
xneos <dcr.xneos@gmail.com>
2012-06-13 12:43:30 +0200

    src
    We miss src folder ;_;

---
+++ src/test.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 13.06.12
+ * Time: 12:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+
+}
