3135a824e2e94fe6152d19024bbf7f1b0143fb35
standy66 <steam.andy@gmail.com>
2012-06-17 13:08:22 +0400

    Added tasks, implemented influenceProcess

---
+++ src/ru/darkcraft/RPGCraft/api/tasks/InfluenceHandlerTask.java
@@ -0,0 +1,14 @@
+package ru.darkcraft.RPGCraft.api.tasks;
+
+/**
+ * Paste description here
+ * <p/>
+ * Created with IntelliJ IDEA.
+ * Date: 17.06.12
+ * Time: 13:03
+ *
+ * @author KarN
+ * @version 1.0
+ */
+public class InfluenceHandlerTask {
+}
---
+++ src/ru/darkcraft/RPGCraft/influences/InstantInfluence.java
@@ -0,0 +1,14 @@
+package ru.darkcraft.RPGCraft.influences;
+
+/**
+ * Paste description here
+ * <p/>
+ * Created with IntelliJ IDEA.
+ * Date: 17.06.12
+ * Time: 12:41
+ *
+ * @author KarN
+ * @version 1.0
+ */
+public class InstantInfluence {
+}
